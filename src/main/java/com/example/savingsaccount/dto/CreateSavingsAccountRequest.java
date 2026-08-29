42eaa6c21c35452eaeb1c116fd610b7e5e1f5e7f
Rajees Patel <rajpnz@gmail.com>
2026-08-30 10:51:40 +1200

    refactor: change package name

--- src/main/java/com/example/savingsaccount/dto/CreateSavingsAccountRequest.java
+++ src/main/java/com/example/savingsaccount/dto/CreateSavingsAccountRequest.java
@@ -4,25 +4,23 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class CreateSavingsAccountRequest {
+
     @NotBlank(message = "customerName must not be blank")
-    private String customerName;
+    private final String customerName;
 
     @Size(min = 5, max = 30, message = "accountNickName must be between 5 and 30 characters")
-    private String accountNickName;
+    private final String accountNickName;
+
+    public CreateSavingsAccountRequest(String customerName, String accountNickName) {
+        this.customerName = customerName;
+        this.accountNickName = accountNickName;
+    }
 
     public String getCustomerName() {
         return customerName;
     }
 
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
     public String getAccountNickName() {
         return accountNickName;
-    }
-
-    public void setAccountNickName(String accountNickName) {
-        this.accountNickName = accountNickName;
     }
 }
