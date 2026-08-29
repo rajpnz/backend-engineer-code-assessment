2e127eec52fd4e478b7144e4ed29566036ce9dc0
Rajees Patel <rajpnz@gmail.com>
2026-08-30 11:26:08 +1200

    feat: add get savings account endpoint

--- src/main/java/com/example/savingsaccount/controller/SavingsAccountController.java
+++ src/main/java/com/example/savingsaccount/controller/SavingsAccountController.java
@@ -4,10 +4,7 @@ import com.example.savingsaccount.dto.CreateSavingsAccountRequest;
 import com.example.savingsaccount.dto.SavingsAccountResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -21,5 +18,11 @@ public class SavingsAccountController {
         SavingsAccountResponse response = new SavingsAccountResponse("xyz", "123", "John Smith", "Johnny");
         URI location = URI.create("/api/v1/savings-accounts/" + response.getId());
         return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<SavingsAccountResponse> getAccount(@PathVariable String accountId) {
+        SavingsAccountResponse response = new SavingsAccountResponse(accountId, "123", "John Smith", "Johnny");
+        return ResponseEntity.ok(response);
     }
 }
