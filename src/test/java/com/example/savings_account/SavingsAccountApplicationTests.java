aa1363fe33c232fcf71ab33005889c541ad86bb1
Rajees Patel <rajpnz@gmail.com>
2026-08-29 18:34:45 +1200

    Initial commit using Spring Initializr

---
+++ src/test/java/com/example/savings_account/SavingsAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savings_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
